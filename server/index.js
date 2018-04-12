05efc5dc5046966def40e5fecb493e7a67130a94
Tharaka Romesh <tharakaromesh@gmail.com>
2018-04-12 18:40:03 +0530

    Add user service and server modifications

--- server/index.js
+++ server/index.js
@@ -1,19 +1,29 @@
-const express = require('express');
-const app =express();
-const path = require('path');
-const server = require('http').Server(app);
-const cors = require('cors')();
-const bodyParser = require('body-parser');
+const express = require("express");
+const app = express();
+const path = require("path");
+const server = require("http").Server(app);
+const cors = require("cors")();
+const bodyParser = require("body-parser");
 
-const port =process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-   extended:true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
 app.use(cors);
 
-app.use('/', express.static(__dirname));
+app.use(express.static(path.join(__dirname, "client/dist")));
 
-app.listen(port,()=> console.log('Running on port: '+port));
+//Add Api
+
+///
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/dist/index.html"));
+});
+
+server.listen(port, () => console.log(`Running on localhost:${port}`));
