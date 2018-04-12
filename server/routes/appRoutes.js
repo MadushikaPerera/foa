4e0ee13bbcb7c752992e3e9add5d4f16340e14d4
Tharaka Romesh <tharakaromesh@gmail.com>
2018-04-12 17:58:56 +0530

    add authentication service

--- server/routes/appRoutes.js
+++ server/routes/appRoutes.js
@@ -1,17 +1,16 @@
-const Authentication = require('../utils/auth');
-const passportService = require('../utils/passport');
-const passport = require('passport');
+const Authentication = require("../utils/auth");
+const passportService = require("../utils/passport");
+const passport = require("passport");
 
-const requireAuth = passport.authenticate('jwt', { session: false });
-const requireSignin = passport.authenticate('local', { session: false });
+const requireAuth = passport.authenticate("jwt", { session: false });
+const requireSignin = passport.authenticate("local", { session: false });
 
-module.exports = function (app) {
-
-  app.get('/hi',function (req, res) {
-    res.send({ hi: 'there' });
+module.exports = function(app) {
+  app.get("/hi", function(req, res) {
+    res.send({ hi: "there" });
   });
 
-  app.post('/signin', requireSignin, Authentication.signin);
+  app.post("/signin", requireSignin, Authentication.signin);
 
-  app.post('/signup', Authentication.signup);
-}
+  app.post("/signup", Authentication.signup);
+};
